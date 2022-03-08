5a625feb69cd99fc28a4dae6f09bd6d339f93278
Ousmane A. Amadou <ousmane.amadou89@gmail.com>
2022-03-08 15:03:59 -0500

    Added some project metadata in kijiji.py

--- kijiji.py
+++ kijiji.py
@@ -1,1 +1,8 @@
-#!/bin/env python3
+import datetime
+from os import lseek
+
+__author__ = "Ousmane Amadou"
+__copyright__ = "Copyright 2022, Ousmane Amadou"
+__license__ = "GPL"
+__version__ = "1.0"
+__date__ = datetime.date.today()
